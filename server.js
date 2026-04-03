c27f3deccf18f626698ca3ec5ff9927893427970
Somya Gupta <somya.gupta01@nagarro.com>
2026-04-03 18:06:33 +0530

    remove dependency

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
-const axios = require('axios');
+
 const app = express();
 
 app.use(express.json());
